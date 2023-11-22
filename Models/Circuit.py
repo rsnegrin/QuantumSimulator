2672ebc23e47abf5a6b81ea2b059e3d48182f1da
Roberto Negrin <rsnegrin@adsl-172-10-2-15.dsl.sndg02.sbcglobal.net>
2023-11-21 23:36:07 -0800

    Single Qubit Gates

--- Models/Circuit.py
+++ Models/Circuit.py
@@ -1,26 +1,38 @@
+import numpy as np
+from Gates import H, X, T, CNOT # Import gates
 
+GATES = {"h": H, 
+         "x": X, 
+         "t": T,
+         'cx': CNOT}
 
 class QuantumCircuit():
     """
     Create a quantum circuit with a given number of qubits and classical bits.
     """
     def __init__(self, qr, cr=0):
-        self.qreg = qr
-        self.creg = cr
-        self.gates = []
+        self.qreg = qr # Number of qubits
+        self.states = np.array([1] + [0] * (2**(qr)-1)) # Initial state
+        self.creg = [0] * cr # Classical register
+        self.gates = [] # Gates
+
     
-    def add_gate(self, gate):
+    def add_gate(self, gate, target, control=None):
         """
         Add a gate to the circuit.
         """
-        self.gates.append(gate)
+        if control:
+            self.gates.append(GATES[gate](self.qreg, control, target))
+        else:
+            self.gates.append(GATES[gate](self.qreg, target))
 
-
-    def run(sefl):
+    def run(self):
         """
         Run the circuit.
         """
-        pass
+        for gate in self.gates:
+            self.states = gate.apply(self.states)
+        return self.states
 
     
     def __str__(self):
@@ -38,3 +50,12 @@ class QuantumCircuit():
 
 
     
+if __name__ == "__main__":
+    # Create a circuit with 2 qubits
+    qc = QuantumCircuit(2)
+    # Add a Hadamard gate on qubit 0
+    qc.add_gate("cx", 0, 1)
+    # Run the circuit
+    print(qc.run())
+    # Print the circuit
+    print(qc)
